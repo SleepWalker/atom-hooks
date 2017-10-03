45cea1f0de27346d6b5ac75e91d35b2139335c84
SleepWalker <mybox@udf.su>
2017-10-03 22:53:50 +0300

    Setup apm to run jest tests. Add travis config

---
+++ test-runner.js
@@ -0,0 +1,25 @@
+'use babel';
+
+import { exec } from 'child_process';
+
+export default function runTests({ logFile }) {
+  if (logFile) {
+    logFile = ` --outputFile='${logFile}'`;
+  } else {
+    logFile = '';
+  }
+
+  return new Promise(resolve => {
+    exec(
+      'npm t --silent -- --ci --json' + logFile,
+      { cwd: __dirname },
+      error => {
+        if (error) {
+          resolve(1);
+        } else {
+          resolve(0);
+        }
+      }
+    );
+  });
+}
